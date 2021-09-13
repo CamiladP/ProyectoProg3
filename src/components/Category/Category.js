d25447430354ff6028f13228ccb570b49506bf0a
macamontoya <56725506+macamontoya@users.noreply.github.com>
2021-09-13 17:36:43 -0300

    Ver mas terminado

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,15 +1,41 @@
-import React , {Component} from "react"
-
-
+import React , {Component} from "react";
+import "./Category.css"
 
 class Category extends Component {
 
     constructor(props){
         super(props)
-        this.state = {}
-    }
-render(){
-return( <article>
+        this.state = {
+            viewMore: false,
+            text: 'Ver más',
+            selected: false,
+        }
+        console.log(this.state);
+    } 
+
+    viewMore() {
+        if(this.state.viewMore){
+        this.setState ({
+            viewMore: false,
+            text: 'Ver más',
+            selected: true, 
+            })
+        } else {
+            this.setState({
+                viewMore: true,
+                text: 'Ver menos',
+                selected: false, 
+                })
+            }
+        }
+    
+   
+   
+
+    render() {
+        return( 
+    <>
+    <article>
     <section className="navigation">
         <div>
             <i className="fas fa-chevron-left"></i>
@@ -21,19 +47,23 @@ return( <article>
         <img src={`https://image.tmdb.org/t/p/w500/${this.props.pelicula.poster_path}`}alt=""/>
         <h3>{this.props.pelicula.title}</h3>
         <h4>Description:</h4>
-        <p className="description">{this.props.pelicula.overview}</p>
+        <p className="description"></p>
         
         <section className="aditional-info">
             <p>Release Date: {this.props.pelicula.release_date}</p>
             <p>Popularity: {this.props.pelicula.popularity}</p>
             
         </section>
-        <a href="">Ver más</a>
+        <p className = {`extra ${this.state.viewMore ? 'show' : 'hide' } `} > {this.props.pelicula.overview} </p>
+        <p className= 'Vermas' onClick={()=> this.viewMore()}> {this.state.text} </p>
     </main>
-</article>)
+    </article>
+    </>
+        );
+    }
+}
 
-}
-}
+
 
 export default Category;
 
